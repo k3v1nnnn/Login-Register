9c2f66a5d9b196fc43850451c2941ff8a0feb479
kevin <kevinmarcelo_km@hotmail.com>
2019-08-06 10:00:07 -0300

    ventana,cosas en el gitignore de eclipse

---
+++ src/Vista/Main.java
@@ -0,0 +1,14 @@
+package Vista;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+public class Main extends Application {
+ public static void main(String[] args) {
+ launch(args);
+ }
+ @Override
+ public void start(Stage stage) throws Exception {
+ stage.setTitle("Mi primera ventana");
+ stage.show();
+ }
+}
