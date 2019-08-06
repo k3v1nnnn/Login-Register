308f587e2e1be3a006125f5483326ce30ab19f41
kevin <kevinmarcelo_km@hotmail.com>
2019-08-06 11:22:02 -0300

    ventana con boton

--- src/Vista/Main.java
+++ src/Vista/Main.java
@@ -1,6 +1,8 @@
 package Vista;
-
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 public class Main extends Application {
  public static void main(String[] args) {
@@ -9,6 +11,12 @@ public class Main extends Application {
  @Override
  public void start(Stage stage) throws Exception {
  stage.setTitle("Mi primera ventana");
+ StackPane layout = new StackPane();
+ Scene scene = new Scene(layout,800,600);
+ stage.setScene(scene);
+ Button button = new Button();
+ button.setText("Texto del botón");
+ layout.getChildren().add(button);
  stage.show();
  }
 }
